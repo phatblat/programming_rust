31bd9d85156145fbddea6c851b785b7fbedd35c4
Ben Chatelain <ben@octop.ad>
2022-01-20 21:53:16 -0700

    ➕ hello crate, use to handle post_gcd

--- actix-gcd/src/main.rs
+++ actix-gcd/src/main.rs
@@ -1,4 +1,7 @@
 use actix_web::{web, App, HttpResponse, HttpServer};
+use serde::Deserialize;
+
+use hello::gcd;
 
 fn main() {
     let server = HttpServer::new(|| {
@@ -25,4 +28,27 @@ fn get_index() -> HttpResponse {
                 </form>
             "#,
         )
+}
+
+#[derive(Deserialize)]
+struct GcdParameters {
+    n: u64,
+    m: u64,
+}
+
+fn post_gcd(form: web::Form<GcdParameters>) -> HttpResponse {
+    if form.n == 0 || form.m == 0 {
+        return HttpResponse::BadRequest()
+            .content_type("text/html")
+            .body("Computing the GCD with zero is boring");
+    }
+
+    let response =
+        format!("The greatest common divisor of the numbers {} and {} \
+                 is <b>{}</b>\n",
+                form.n, form.m, gcd(form.n, form.m));
+
+    HttpResponse::Ok()
+        .content_type("text/html")
+        .body(response)
 }
