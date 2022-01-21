0757f4fbbb7b11d0d470104911c0ea40f6ee850f
Ben Chatelain <ben@octop.ad>
2022-01-20 21:07:38 -0700

    ♻️ Split out gcd fn into lib.rs

--- hello/src/main.rs
+++ hello/src/main.rs
@@ -1,6 +1,8 @@
 use std::str::FromStr;
 use std::env;
 
+mod lib;
+
 fn main() {
     let mut numbers = Vec::new();
 
@@ -16,31 +18,9 @@ fn main() {
 
     let mut d = numbers[0];
     for m in &numbers[1..] {
-        d = gcd(d, *m);
+        d = lib::gcd(d, *m);
     }
 
     println!("The greatest common divison of {:?} is {}",
              numbers, d);
-}
-
-fn gcd(mut n: u64, mut m: u64) -> u64 {
-    assert!(n != 0 && m != 0);
-    while m != 0 {
-        if m < n {
-            let t = m;
-            m = n;
-            n = t;
-        }
-        m = m % n;
-    }
-    n
-}
-
-#[test]
-fn test_gcd() {
-    assert_eq!(gcd(14, 15), 1);
-
-    assert_eq!(gcd(2 * 3 * 5 * 11 * 17,
-                   3 * 7 * 11 * 13 * 19),
-               3 * 11);
 }
