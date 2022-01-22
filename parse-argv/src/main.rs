8cc048c307c2628ce65d44b0edde39b7b291b64b
Ben Chatelain <ben@octop.ad>
2022-01-22 12:40:16 -0700

    💚 Add parse_complex and test

--- parse-argv/src/main.rs
+++ parse-argv/src/main.rs
@@ -1,4 +1,5 @@
 use std::str::FromStr;
+use num::Complex;
 
 fn main() {
     println!("Hello, world!");
@@ -34,4 +35,19 @@ fn test_parse_pair() {
     assert_eq!(parse_pair::<i32>("10,20xy", ','), None);
     assert_eq!(parse_pair::<f64>("0.5x",    'x'), None);
     assert_eq!(parse_pair::<f64>("0.5x1.5", 'x'), Some((0.5, 1.5)));
+}
+
+/// Parse a pair of floating-point numbers separated by a comma as a complex number.
+fn parse_complex(s: &str) -> Option<Complex<f64>> {
+    match parse_pair(s, ',') {
+        Some((re, im)) => Some(Complex { re, im }),
+        None => None
+    }
+}
+
+#[test]
+fn test_parse_complex() {
+    assert_eq!(parse_complex("1.25,-0.0625"),
+               Some(Complex { re: 1.25, im: -0.0625 }));
+    assert_eq!(parse_complex(",-0.0625"), None);
 }
