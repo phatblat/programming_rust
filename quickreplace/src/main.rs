2a5101cd28d2aa908f4095e1be65061fe214b552
Ben Chatelain <ben@octop.ad>
2022-01-23 10:38:06 -0700

    ✨ Add replacement logic and input, output files

--- quickreplace/src/main.rs
+++ quickreplace/src/main.rs
@@ -1,3 +1,4 @@
+use regex::Regex;
 use std::env;
 use std::fs;
 use text_colorizer::*;
@@ -22,8 +23,17 @@ fn main() {
         }
     };
 
-    match fs::write(&args.output, &data) {
-        Ok(_) => {},
+    let replaced_data = match replace(&args.target, &args.replacement, &data) {
+        Ok(v) => v,
+        Err(e) => {
+            eprintln!("{} failed to replace text: {:?}",
+                "Error:".red().bold(), e);
+            std::process::exit(1);
+        }
+    };
+
+    match fs::write(&args.output, &replaced_data) {
+        Ok(v) => v,
         Err(e) => {
             eprintln!("{} failed to write to file '{}': {:?}",
                 "Error:".red().bold(), args.filename, e);
@@ -54,4 +64,11 @@ fn parse_args() -> Arguments {
         filename: args[2].clone(),
         output: args[3].clone(),
     }
+}
+
+fn replace(target: &str, replacement: &str, text: &str)
+    -> Result<String, regex::Error>
+{
+    let regex = Regex::new(target)?;
+    Ok(regex.replace_all(text, replacement).to_string())
 }
